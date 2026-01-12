6f637c0907a6d8d4cff20e1ffab15a8e3f91b5af
handleui <rodrigo@handleui.com>
2026-01-11 21:20:41 -0600

    test: trigger #4

--- test-error.ts
+++ test-error.ts
@@ -16,3 +16,6 @@ const arr: number[] = "not an array";
 // Even more errors to trigger CI
 const obj: { name: string } = { name: 123 };
 const fn: () => void = "not a function";
+
+// Trigger #4
+const tuple: [string, number] = [1, "wrong order"];
